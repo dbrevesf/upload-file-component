84cbef45f0ecc1d020644979a4f71661a4364a1f
Daniel Breves Ferreira <dbrevesf@gmail.com>
2020-08-20 16:45:45 -0300

    Changing message style according to error

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -2,14 +2,16 @@ import React from "react";
 
 type MessageProps = {
   msg: String;
+  isError: boolean;
 };
 
 const Message: React.FC<MessageProps> = (messageProps) => {
-  const { msg } = messageProps;
+  const { msg, isError } = messageProps;  
+  const alertSyle = isError ? 'alert-danger' : 'alert-info';
   return (
     <div>
       <div
-        className="alert alert-danger alert-dismissible fade show"
+        className={`alert ${alertSyle} alert-dismissible fade show`}
         role="alert"
       >
         {msg}
