517d89bfc8a066411d43ab02adec38bdfe59565e
Daniel Breves Ferreira <dbrevesf@gmail.com>
2020-08-20 11:51:54 -0300

    Using useEffect to use the importName and make the upload on onSubmit

--- client/src/components/FileUpload.tsx
+++ client/src/components/FileUpload.tsx
@@ -1,53 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Message from "./Message";
 import ProgressBar from "./ProgressBar";
 import Resumable from "resumablejs";
 
 const FileUpload: React.FC = () => {
 
-  const [file, setFile] = useState<Resumable.ResumableFile>();
+  const [file, setFile] = useState<File>();
   const [fileName, setFileName] = useState("Choose File");
-  const [uploadedFile, setUploadedFile] = useState({
-    fileName: "",
-    filePath: "",
-  });
+  const [uploadedFile, setUploadedFile] = useState({fileName: "",filePath: "",});
   const [message, setMessage] = useState("");
   const [uploadPercentage, setUploadPercentage] = useState(0);
   const [importName, setImportName] = useState('Set an import name. Ex: modeldata');
   const [uploaderRef, setUploaderRef] = useState<HTMLInputElement | null | undefined>();
-  
-  const r = new Resumable({
-    target: 'http://localhost:3000/upload',
-    chunkSize: 1024 * 1024 * 5,
-    maxFiles: 1,
-    query: {importName: "generic name"}
-  });
-  
-  if(!r.support) {
-    console.log('nao Rolou');
-    setMessage('Resumable is not installed!');
-  } else {    
-    console.log('Rolou');  
-    if (uploaderRef) {
-      console.log("Toaki")
-      r.assignBrowse(uploaderRef, false);
-      r.on('fileAdded', (file, event) => {
-        r.upload();
-        setFile(file);
-        setFileName(file.fileName);
-        console.log('FileAdded');
+  const resumable = useRef<Resumable>();
 
-        console.log(file);
-      });
-      r.on('fileError', (file, message) => {
-        console.log(file.fileName);
-        console.log(message);
-      });
-      r.on('fileSuccess', (file) => {
-        console.log('Sucesso');
-      })
+  useEffect(() => {
+    const r = new Resumable({
+      target: 'http://localhost:3000/upload',
+      chunkSize: 1024 * 1024 * 5,
+      maxFiles: 1,
+      query: {importName: importName}
+    });
+    
+    if(!r.support) {
+      setMessage('Resumable is not installed!');
+    } else {    
+      console.log('Rolou');  
+      if (uploaderRef) {
+        r.assignBrowse(uploaderRef, false);
+        r.on('fileAdded', (file, event) => {
+          setFile(file.file);
+          setFileName(file.fileName);
+          console.log('FileAdded');
+        });
+        r.on('fileError', (file, message) => {
+          console.log(file.fileName);
+          console.log(message);
+        });
+        r.on('fileSuccess', (file) => {
+          console.log('Success');
+        });
+      }
     }
-  }
+    if (resumable.current && r.files.length === 0) {
+      if (file) {
+        r.addFile(file);  
+      }
+    }
+    resumable.current = r;
+  }, [importName, uploaderRef, file])
+  
+
+  
 
   const onChangeImportName = (e: any) => {
     setImportName(e.target.value);
@@ -55,8 +59,10 @@ const FileUpload: React.FC = () => {
 
   const onSubmit = async (e: any) => {
     e.preventDefault();
-    console.log(file);
-    console.log("CHAMEI O UPLOAD")
+    if (resumable.current) {
+      resumable.current.upload();
+    }
+    
     // const formData = new FormData();
     // formData.append("file", file);
     // try {
@@ -86,53 +92,6 @@ const FileUpload: React.FC = () => {
   };
 
   
-
-  // Resumable.js isn't supported, fall back on a different method
-  // if(!r.support) {
-  //   $('.resumable-error').show();
-  // } else {
-  //   // Show a place for dropping/selecting files
-  //   $('.resumable-drop').show();
-  //   r.assignDrop($('.resumable-drop')[0]);
-  //   r.assignBrowse($('.resumable-browse')[0]);
-
-  //   // Handle file add event
-  //   r.on('fileAdded', function(file){
-  //       // Show progress pabr
-  //       $('.resumable-progress, .resumable-list').show();
-  //       // Show pause, hide resume
-  //       $('.resumable-progress .progress-resume-link').hide();
-  //       $('.resumable-progress .progress-pause-link').show();
-  //       // Add the file to the list
-  //       $('.resumable-list').append('<li class="resumable-file-'+file.uniqueIdentifier+'">Uploading <span class="resumable-file-name"></span> <span class="resumable-file-progress"></span>');
-  //       $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-name').html(file.fileName);
-  //       // Actually start the upload
-  //       r.upload();
-  //     });
-  //   r.on('pause', function(){
-  //       // Show resume, hide pause
-  //       $('.resumable-progress .progress-resume-link').show();
-  //       $('.resumable-progress .progress-pause-link').hide();
-  //     });
-  //   r.on('complete', function(){
-  //       // Hide pause/resume when the upload has completed
-  //       $('.resumable-progress .progress-resume-link, .resumable-progress .progress-pause-link').hide();
-  //     });
-  //   r.on('fileSuccess', function(file,message){
-  //       // Reflect that the file upload has completed
-  //       $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-progress').html('(completed)');
-  //     });
-  //   r.on('fileError', function(file, message){
-  //       // Reflect that the file upload has resulted in error
-  //       $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-progress').html('(file could not be uploaded: '+message+')');
-  //     });
-  //   r.on('fileProgress', function(file){
-  //       // Handle progress for both the file and the overall upload
-  //       $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-progress').html(Math.floor(file.progress()*100) + '%');
-  //       $('.progress-bar').css({width:Math.floor(r.progress()*100) + '%'});
-  //     });
-  // }
-
   return (
     <>
       {message ? <Message msg={message} /> : null}
@@ -143,7 +102,6 @@ const FileUpload: React.FC = () => {
             className="custom-file-input"
             id="customFile"
             name='customFile'
-            // onChange={onChange}
             ref={node => setUploaderRef(node)}
           />
           <label className="custom-file-label" htmlFor="customFile">
